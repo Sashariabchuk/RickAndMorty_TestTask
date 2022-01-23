aa734e29db0fe777402ac484b59bbe2a293b199d
Sasha Riabchuk <oleksandr.riabchuk@skoda-auto.cz>
2022-01-23 20:47:32 +0100

    init commit

---
+++ RickAndMorty_TestTask/RickAndMorty_TestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMorty_TestTaskApp.swift
+//  RickAndMorty_TestTask
+//
+//  Created by Sasha Riabchuk on 23.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMorty_TestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
